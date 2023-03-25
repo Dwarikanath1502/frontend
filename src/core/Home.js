d412980a19ed082c7ee8deca4d89ec3d26c287ef
Dwarikanath1502 <ddwarikanath15@gmail.com>
2023-03-26 01:06:10 +0530

    added navigation to home and CSS

---
+++ src/core/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles.css'
+
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className='text-white'>Hello front   end</h1>
+    </div>
+  )
+}
+
+export default Home
