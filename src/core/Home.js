11a73a0473b23b02600928be535efa80ca327f29
Dwarikanath1502 <ddwarikanath15@gmail.com>
2023-03-26 18:57:07 +0530

    created environment file and created Base component

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,12 +1,29 @@
 import React from 'react'
+import { API } from '../backend';
 import '../styles.css'
+import Base from './Base';
 
 
 const Home = () => {
+  console.log("API IS: ", API);
   return (
-    <div>
-      <h1 className='text-white'>Hello front   end</h1>
-    </div>
+    <Base title="Homepage">
+      <h1 className='text-white'>Hello front end</h1>
+      <div className="row">
+        <div className="col-4">
+          <button className="btn btn-success">TEST</button>
+        </div>
+        <div className="col-4">
+          <button className="btn btn-success">TEST</button>
+
+        </div>
+        <div className="col-4">
+          <button className="btn btn-success">TEST</button>
+
+        </div>
+
+      </div>
+    </Base>
   )
 }
 
