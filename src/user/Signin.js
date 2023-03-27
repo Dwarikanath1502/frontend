26f400ad7cc6563772ca6e25ae4ced14e4b36a93
Dwarikanath1502 <ddwarikanath15@gmail.com>
2023-03-27 16:10:24 +0530

    created signin and signup page

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -1,14 +1,34 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Base from '../core/Base'
 
 const Signin = () => {
-  return (
-    <Base title='sign in page' description='A page for user to sign in!'>
-        <h1>Sign in works!</h1>
-    </Base>
-  )
+
+    const SigninForm = () => {
+        return (
+            <div className="row">
+                <div className="col-md-6 offset-sm-3 text-left">
+                    <form >
+                        <div className="form-group">
+                            <label className="text-light"> Name</label>
+                            <input className='form-control' type="text" />
+                        </div>
+                        <div className="form-group">
+                            <label className="text-light"> Password</label>
+                            <input className='form-control' type="password" />
+                        </div>
+                        <button className="btn btn-lg btn-success mt-2">Submit</button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <Base title='sign in page' description='A page for user to sign in!'>
+            {SigninForm()}
+        </Base>
+    )
 }
 
 export default Signin
- 
--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,14 +1,38 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Base from '../core/Base'
 
 const Signup = () => {
-  return (
-    <Base title='sign up page' description='A page for user to sign up!'>
-        <h1>Sign up works!</h1>
-    </Base>
-  )
+
+    const SignupForm = () => {
+        return (
+            <div className="row">
+                <div className="col-md-6 offset-sm-3 text-left">
+                    <form >
+                        <div className="form-group">
+                            <label className="text-light"> Name</label>
+                            <input className='form-control' type="text" />
+                        </div>
+                        <div className="form-group">
+                            <label className="text-light"> Email</label>
+                            <input className='form-control' type="email" />
+                        </div>
+                        <div className="form-group">
+                            <label className="text-light"> Password</label>
+                            <input className='form-control' type="password" />
+                        </div>
+                        <button className="btn btn-lg btn-success mt-2">Submit</button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <Base title='sign up page' description='A page for user to sign up!'>
+            {SignupForm()}
+        </Base>
+    )
 }
 
 export default Signup
- 
